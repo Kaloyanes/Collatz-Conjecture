5a58542081913d3b84b99a685945dcd28f863ba4
Kaloyan Stoyanov <kaloyangfx@gmail.com>
2021-12-02 23:43:09 +0200

    Count to 1 Button added

--- Collatz Conjecture/Main.Designer.cs
+++ Collatz Conjecture/Main.Designer.cs
@@ -33,12 +33,17 @@
             this.btnGenerate = new System.Windows.Forms.Button();
             this.CCChart = new LiveCharts.WinForms.CartesianChart();
             this.lblLimit = new System.Windows.Forms.Label();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.cmboxNumbers = new System.Windows.Forms.ComboBox();
+            this.btnDark = new System.Windows.Forms.Button();
+            this.btnLight = new System.Windows.Forms.Button();
+            this.btnCount = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // lblN
             // 
             this.lblN.AutoSize = true;
-            this.lblN.Location = new System.Drawing.Point(12, 9);
+            this.lblN.Location = new System.Drawing.Point(12, 11);
             this.lblN.Name = "lblN";
             this.lblN.Size = new System.Drawing.Size(27, 15);
             this.lblN.TabIndex = 0;
@@ -46,7 +51,7 @@
             // 
             // txtN
             // 
-            this.txtN.Location = new System.Drawing.Point(45, 6);
+            this.txtN.Location = new System.Drawing.Point(45, 8);
             this.txtN.Name = "txtN";
             this.txtN.Size = new System.Drawing.Size(182, 23);
             this.txtN.TabIndex = 2;
@@ -68,31 +73,92 @@
             this.CCChart.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.CCChart.Location = new System.Drawing.Point(12, 35);
+            this.CCChart.Location = new System.Drawing.Point(12, 50);
             this.CCChart.Name = "CCChart";
-            this.CCChart.Size = new System.Drawing.Size(1153, 508);
+            this.CCChart.Size = new System.Drawing.Size(1293, 541);
             this.CCChart.TabIndex = 4;
             this.CCChart.Text = "CCChart";
             // 
             // lblLimit
             // 
             this.lblLimit.AutoSize = true;
-            this.lblLimit.Location = new System.Drawing.Point(324, 10);
+            this.lblLimit.Location = new System.Drawing.Point(558, 11);
             this.lblLimit.Name = "lblLimit";
             this.lblLimit.Size = new System.Drawing.Size(40, 15);
             this.lblLimit.TabIndex = 5;
             this.lblLimit.Text = "Limit: ";
             // 
+            // btnClear
+            // 
+            this.btnClear.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnClear.Location = new System.Drawing.Point(1230, 7);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 23);
+            this.btnClear.TabIndex = 8;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // cmboxNumbers
+            // 
+            this.cmboxNumbers.AllowDrop = true;
+            this.cmboxNumbers.FormattingEnabled = true;
+            this.cmboxNumbers.Location = new System.Drawing.Point(749, 8);
+            this.cmboxNumbers.Name = "cmboxNumbers";
+            this.cmboxNumbers.Size = new System.Drawing.Size(222, 23);
+            this.cmboxNumbers.TabIndex = 10;
+            // 
+            // btnDark
+            // 
+            this.btnDark.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnDark.Location = new System.Drawing.Point(1149, 7);
+            this.btnDark.Name = "btnDark";
+            this.btnDark.Size = new System.Drawing.Size(75, 23);
+            this.btnDark.TabIndex = 11;
+            this.btnDark.Text = "Dark Mode";
+            this.btnDark.UseVisualStyleBackColor = true;
+            this.btnDark.Click += new System.EventHandler(this.btnDark_Click);
+            // 
+            // btnLight
+            // 
+            this.btnLight.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnLight.Location = new System.Drawing.Point(1149, 7);
+            this.btnLight.Name = "btnLight";
+            this.btnLight.Size = new System.Drawing.Size(75, 23);
+            this.btnLight.TabIndex = 12;
+            this.btnLight.Text = "Light Mode";
+            this.btnLight.UseVisualStyleBackColor = true;
+            this.btnLight.Visible = false;
+            this.btnLight.Click += new System.EventHandler(this.btnLight_Click);
+            // 
+            // btnCount
+            // 
+            this.btnCount.AutoSize = true;
+            this.btnCount.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.btnCount.Location = new System.Drawing.Point(303, 6);
+            this.btnCount.Name = "btnCount";
+            this.btnCount.Size = new System.Drawing.Size(73, 25);
+            this.btnCount.TabIndex = 13;
+            this.btnCount.Text = "Count to 1";
+            this.btnCount.UseVisualStyleBackColor = true;
+            this.btnCount.Click += new System.EventHandler(this.btnCount_Click);
+            // 
             // Main
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
-            this.ClientSize = new System.Drawing.Size(1177, 555);
+            this.ClientSize = new System.Drawing.Size(1317, 603);
+            this.Controls.Add(this.btnCount);
+            this.Controls.Add(this.btnLight);
+            this.Controls.Add(this.btnDark);
+            this.Controls.Add(this.cmboxNumbers);
+            this.Controls.Add(this.btnClear);
             this.Controls.Add(this.lblLimit);
             this.Controls.Add(this.CCChart);
             this.Controls.Add(this.btnGenerate);
             this.Controls.Add(this.txtN);
             this.Controls.Add(this.lblN);
+            this.MinimumSize = new System.Drawing.Size(1333, 642);
             this.Name = "Main";
             this.Text = "Collatz Conjecture";
             this.ResumeLayout(false);
@@ -107,5 +173,10 @@
         private Button btnGenerate;
         private LiveCharts.WinForms.CartesianChart CCChart;
         private Label lblLimit;
+        private Button btnClear;
+        private ComboBox cmboxNumbers;
+        private Button btnDark;
+        private Button btnLight;
+        private Button btnCount;
     }
 }
--- Collatz Conjecture/Main.cs
+++ Collatz Conjecture/Main.cs
@@ -2,28 +2,24 @@ using System.Windows.Media;
 using System.Windows.Media.Animation;
 using LiveCharts;
 using LiveCharts.Wpf;
+using Color = System.Drawing.Color;
 
 namespace Collatz_Conjecture
 {
     public partial class Main : Form
     {
-        private Int64 n;
-        private Int64[] errorInts = new Int64[] { 0, 1, 2, 4 };
+        private long n;
+        private long[] errorInts = new long[] { 0, 1, 2, 4 };
         private int steps;
         private Axis axisX;
         private Axis axisY;
+        private List<long> numbers = new List<long>();
 
         public Main()
         {
             InitializeComponent();
+            CCChart.Zoom = ZoomingOptions.Y;
             CCChart.Series = new SeriesCollection();
-            CCChart.Series.Add(new LineSeries()
-            {
-                Values = new ChartValues<Int64>(),
-                PointGeometrySize = 15,
-                LineSmoothness = 0.5,
-                DataLabels = false
-            });
 
             axisX = new Axis()
             {
@@ -33,24 +29,37 @@ namespace Collatz_Conjecture
 
             CCChart.AxisX.Add(axisX);
 
-            lblLimit.Text += Int64.MaxValue;
+            lblLimit.Text += long.MaxValue;
         }
 
         private void btnGenerate_Click(object sender, EventArgs e)
         {
             // Resetting the values
-            n = Int64.Parse(txtN.Text);
+            n = long.Parse(txtN.Text);
+            long nCopy = n;
             steps = 0;
 
-            if (errorInts.Contains(n))
+            if (numbers.Contains(n))
             {
-                MessageBox.Show($"It can't be {n}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                MessageBox.Show("Can't repeat numbers", "Repeated number", MessageBoxButtons.OK, MessageBoxIcon.Error);
                 return;
             }
 
-            CCChart.Series[0].Values.Clear();
+            numbers.Add(n);
+
+            CCChart.Series.Add(new LineSeries()
+            {
+                Title = $"#{n}",
+                Values = new ChartValues<long>(),
+                PointGeometrySize = 15,
+                LineSmoothness = 0
+            });
+
+            int series = CCChart.Series.Count - 1;
+
+            CCChart.Series[series].Values.Clear();
             CCChart.AxisX[0].Labels.Clear();
-            CCChart.Series[0].Values.Add(n);
+            CCChart.Series[series].Values.Add(n);
             CCChart.AxisX[0].Labels.Add($"Step: {steps}");
 
             // Doing the Collatz Conjecture
@@ -63,9 +72,100 @@ namespace Collatz_Conjecture
 
                 steps++;
 
-                CCChart.Series[0].Values.Add(n);
+                CCChart.Series[series].Values.Add(n);
                 CCChart.AxisX[0].Labels.Add($"Step: {steps}");
             }
+
+            string max = $"Max: {CCChart.Series[series].Values.Cast<long>().Max()}";
+            string stepsMsg = $"Steps: {steps}";
+            cmboxNumbers.Items.Add($"#{nCopy} | {max} {stepsMsg}");
+            cmboxNumbers.SelectedIndex = series;
+            txtN.Text = "";
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            foreach (var chart in CCChart.Series)
+            {
+                chart.Values.Clear();
+            }
+            cmboxNumbers.Items.Clear();
+            cmboxNumbers.Text = "";
+            numbers.Clear();
+        }
+
+        private void btnDark_Click(object sender, EventArgs e)
+        {
+            CCChart.BackColor = Color.Black;
+            CCChart.ForeColor = Color.White;
+            ActiveForm.BackColor = Color.Black;
+            ActiveForm.ForeColor = Color.White;
+            btnLight.Show();
+            btnDark.Hide();
+        }
+
+        private void btnLight_Click(object sender, EventArgs e)
+        {
+            CCChart.BackColor = Color.White;
+            CCChart.ForeColor = Color.Black;
+            ActiveForm.BackColor = Color.White;
+            ActiveForm.ForeColor = Color.Black;
+            btnLight.Hide();
+            btnDark.Show();
+        }
+
+        private void btnCount_Click(object sender, EventArgs e)
+        {
+            int n2 = int.Parse(txtN.Text);
+            for (int i = n2; i > 0; i--)
+            {
+                long n = long.Parse(i.ToString());
+                long nCopy = n;
+                steps = 0;
+
+                if (numbers.Contains(n))
+                {
+                    MessageBox.Show("Can't repeat numbers", "Repeated number", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+
+                numbers.Add(n);
+
+                CCChart.Series.Add(new LineSeries()
+                {
+                    Title = $"#{n}",
+                    Values = new ChartValues<long>(),
+                    PointGeometrySize = 15,
+                    LineSmoothness = 0
+                });
+
+                int series = CCChart.Series.Count - 1;
+
+                CCChart.Series[series].Values.Clear();
+                CCChart.AxisX[0].Labels.Clear();
+                CCChart.Series[series].Values.Add(n);
+                CCChart.AxisX[0].Labels.Add($"Step: {steps}");
+
+                // Doing the Collatz Conjecture
+                while (n != 1)
+                {
+                    if (n % 2 == 0)
+                        n /= 2;
+                    else
+                        n = n * 3 + 1;
+
+                    steps++;
+
+                    CCChart.Series[series].Values.Add(n);
+                    CCChart.AxisX[0].Labels.Add($"Step: {steps}");
+                }
+
+                string max = $"Max: {CCChart.Series[series].Values.Cast<long>().Max()}";
+                string stepsMsg = $"Steps: {steps}";
+                cmboxNumbers.Items.Add($"#{nCopy} | {max} {stepsMsg}");
+                cmboxNumbers.SelectedIndex = series;
+            }
+            txtN.Text = "";
         }
     }
 }
